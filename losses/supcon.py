1fe4b56475646c193bf29bbd159e2d7b917e11e1
lukasellinger <lu.ellinger@gmx.de>
2024-04-25 14:46:15 +0200

    feat: supcon loss

---
+++ losses/supcon.py
@@ -0,0 +1,46 @@
+"""Module for supervised contrastive loss.
+ref: https://arxiv.org/abs/2004.11362"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import tensor
+
+
+class SupConLoss(nn.Module):
+    """Supervised Contrastive Loss."""
+    def __init__(self, temperature=0.5):
+        super(SupConLoss, self).__init__()
+        self.temperature = temperature  # higher temperature leads to lower loss
+
+    def forward(self, anchor: tensor, references: tensor, labels: tensor) -> tensor:
+        """
+        Calculate the mean supervised contrastive loss over a batch. Each entry of the batch
+        can have a different amount of positives and negatives. These are marked with the labels.
+        Positives are marked with 1, Negatives with 0. Padding with -1.
+        :param anchor: tensor of shape (b x 1 x d)
+        :param references: tensor of shape (b x r x d)
+        :param labels: tensor of shape (b x r)
+        :return: mean batch loss
+        """
+        pos_count = torch.sum(torch.eq(labels, 1), dim=-1)
+        similarity = F.cosine_similarity(anchor, references, dim=-1) / self.temperature
+
+        logits_max = torch.max(similarity.nan_to_num(nan=float('-inf')), dim=-1)[0].detach()
+        similarity = similarity - logits_max.unsqueeze(-1)
+
+        pos_pairs = similarity.masked_fill(~torch.eq(labels, 1), 0)
+        sum_pos_pairs = torch.sum(pos_pairs)
+
+        neg_pairs = similarity.masked_fill(~torch.eq(labels, 0), float('-inf'))
+        pos_pairs = similarity.masked_fill(~torch.eq(labels, 1), float('-inf'))
+
+        exp_pos_pairs = torch.exp(pos_pairs)
+        exp_neg_pairs = torch.exp(neg_pairs)
+        exp_total = torch.sum(exp_pos_pairs + exp_neg_pairs, dim=-1)
+        log_exp_total = torch.log(exp_total)
+
+        loss = - 1 / pos_count * (sum_pos_pairs - pos_count * log_exp_total)
+
+        # In entries with more pos elements, the impact of each  pos element is smaller compared to
+        # entries with fewer pos elements.
+        return loss.mean()
--- main_evidence_selection.py
+++ main_evidence_selection.py
@@ -1,14 +1,14 @@
 """Main evidence selection script."""
 import torch
 from datasets import Dataset
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score, f1_score, recall_score
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModel, AutoModelForMaskedLM, BigBirdModel
-import matplotlib.pyplot as plt
 
 from config import DB_URL
 from dataset.def_dataset import DefinitionDataset
+from losses.supcon import SupConLoss
 from models.evidence_selection_model import EvidenceSelectionModel
 import torch.nn.functional as F
 
@@ -18,7 +18,7 @@ dataset = Dataset.from_sql("""select dd.id, dd.claim, dd.label, docs.document_id
                                          docs.lines, group_concat(dd.evidence_sentence_id) as evidence_lines
                                   from def_dataset dd
                                     join documents docs on docs.document_id = dd.evidence_wiki_url
-                                  where set_type='train' and length(docs.text) < 800
+                                  where set_type='train' and length(docs.text) < 100
                                   group by dd.id, evidence_annotation_id, evidence_wiki_url
                                   limit 30""",
                            con=DB_URL)
@@ -36,6 +36,7 @@ train_dataset = DefinitionDataset(dataset, tokenizer, mode='validation', model='
 train_dataloader = DataLoader(train_dataset, shuffle=True,
                               collate_fn=train_dataset.collate_fn,
                               batch_size=10)
+criterion = SupConLoss()
 
 
 def convert_to_labels(similarities, labels, k=2):
@@ -43,14 +44,18 @@ def convert_to_labels(similarities, labels, k=2):
     predicted = torch.zeros_like(similarities)
     predicted.scatter_(1, top_indices, 1)
 
+    top_k_hits = labels[torch.arange(10).unsqueeze(1), top_indices]
+    top_k_hits = torch.any(top_k_hits == 1, dim=1).float()
+
     mask = (labels != -1).flatten()
-    return predicted.flatten()[mask], labels.flatten()[mask]
+    return predicted.flatten()[mask], labels.flatten()[mask], top_k_hits
 
 
 gt_labels = []
 pr_labels = []
 claim_lenghts = []
 doc_lenghts = []
+all_top_k_hits = []
 for batch in tqdm(train_dataloader):
     selection_model.eval()
     with torch.no_grad():
@@ -60,12 +65,16 @@ for batch in tqdm(train_dataloader):
         sentence_embeddings = selection_model(input_ids=model_input['input_ids'],
                                               attention_mask=model_input['attention_mask'],
                                               sentence_mask=model_input['sentence_mask'])
+
+        loss = criterion(claim_embedding, sentence_embeddings, labels=batch['labels'])
+
         claim_similarities = F.cosine_similarity(claim_embedding, sentence_embeddings, dim=2)
         claim_similarities = claim_similarities.nan_to_num(nan=float('-inf'))
 
-    predicted, true_labels = convert_to_labels(claim_similarities, batch['labels'], k=2)
+    predicted, true_labels, top_k_hits = convert_to_labels(claim_similarities, batch['labels'], k=3)
     gt_labels.extend(true_labels.tolist())
     pr_labels.extend(predicted.tolist())
+    all_top_k_hits.extend(top_k_hits.tolist())
 
     if 'claim_length' in batch and 'doc_length' in batch:
         def extend_to_labels(values, labels) -> list[int]:
@@ -77,8 +86,10 @@ for batch in tqdm(train_dataloader):
         doc_lenghts.extend(extend_to_labels(batch['doc_length'], batch['labels']))
 
 acc = accuracy_score(gt_labels, pr_labels)
+recall = recall_score(gt_labels, pr_labels)
 f1_weighted = f1_score(gt_labels, pr_labels, average='weighted')
 f1_macro = f1_score(gt_labels, pr_labels, average='macro')
+top_k_acc = sum(all_top_k_hits) / len(train_dataset)
 
 if len(claim_lenghts) > 0:
     bin_stats = calc_bin_stats(gt_labels, pr_labels, claim_lenghts)
@@ -93,5 +104,7 @@ if len(claim_lenghts) > 0:
                x_label='Doc Length', y_label='Acc')
 
 print(acc)
+print(recall)
 print(f1_weighted)
 print(f1_macro)
+print(top_k_acc)
