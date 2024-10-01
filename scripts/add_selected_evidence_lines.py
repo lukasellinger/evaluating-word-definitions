7023e13d87cacc59c2023687b1b25868b0ef4a85
lukasellinger <lu.ellinger@gmx.de>
2024-10-01 13:41:47 +0200

    fix: add_selected_evidence_lines

--- scripts/add_selected_evidence_lines.py
+++ scripts/add_selected_evidence_lines.py
@@ -7,62 +7,58 @@ from dataset.def_dataset import split_text, process_lines
 from general_utils.utils import process_sentence_wiki
 from pipeline_module.evidence_selector import ModelEvidenceSelector
 
+model_name = 'lukasellinger/evidence_selection_model-v4'
+dataset_dict = load_dataset('lukasellinger/fever_claim_verification_dissim-v1')
 
-def main():
-    model_name = 'lukasellinger/evidence_selection_model-v4'
-    dataset_dict = load_dataset('lukasellinger/fever_claim_verification_dissim-v1')
+evid_selector = ModelEvidenceSelector(model_name, evidence_selection='top', min_similarity=0)
 
-    evid_selector = ModelEvidenceSelector(model_name, evidence_selection='top')
+for name, dataset in dataset_dict.items():
+    selected_evidence_lines = []
+    for entry in tqdm(dataset):
+        try:
+            lines = process_lines(entry['lines'])
+            processed_lines = []
+            line_numbers = []
+            for line in lines.split('\n'):
+                line = process_sentence_wiki(line)
+                line_number, text = split_text(line)
+                processed_lines.append(text)
+                line_numbers.append(line_number)
+            evidence = [{'title': entry['document_id'],
+                         'line_indices': line_numbers,
+                         'lines': processed_lines}]
 
-    for name, dataset in dataset_dict.items():
-        selected_evidence_lines = []
-        for entry in tqdm(dataset):
-            try:
-                lines = process_lines(entry['lines'])
-                processed_lines = []
-                line_numbers = []
-                for line in lines.split('\n'):
-                    line = process_sentence_wiki(line)
-                    line_number, text = split_text(line)
-                    processed_lines.append(text)
-                    line_numbers.append(line_number)
-                evidence = [{'title': entry['document_id'],
-                             'line_indices': line_numbers,
-                             'lines': processed_lines}]
+            evids_batch = evid_selector.select_evidences({'text': entry['claim']}, evidence)
 
-                evids_batch = evid_selector.select_evidences({'text': entry['claim']}, evidence)
+            pr_evidence_lines = [evidence.get('line_idx') for evidence in evids_batch]
+            if gold_evidence_lines := entry['evidence_lines']:
+                evidence_groups = gold_evidence_lines.split(';')
+                pr_useable = False
+                for group in evidence_groups:
+                    if all(line in pr_evidence_lines for line in group.split(',')):
+                        pr_useable = True
+                        break
+                if not pr_useable:  # no gold evidence in predicted evidence
+                    print(f"Claim predictions false: {entry['claim']}")
+                    builded_ev_lines = []
+                    while len(builded_ev_lines) < 3:
+                        if len(evidence_groups) == 0:
+                            while len(builded_ev_lines) < 3:
+                                line = pr_evidence_lines.pop()
+                                if line not in builded_ev_lines:
+                                    builded_ev_lines.append(line)
+                        else:
+                            group = random.choice(evidence_groups)
+                            evidence_groups.remove(group)
+                            group = group.split(',')
+                            if len(builded_ev_lines) + len(group) <= 3:
+                                builded_ev_lines.extend(group)
+                    pr_evidence_lines = builded_ev_lines
+            selected_evidence_lines.append(','.join(pr_evidence_lines))
+        except Exception as e:
+            print(f"Exception at {entry['claim']}")
+    dataset = dataset.remove_columns('selected_evidence_lines')
+    dataset = dataset.add_column('selected_evidence_lines', selected_evidence_lines)
+    dataset_dict[name] = dataset
 
-                pr_evidence_lines = [evidence.get('line_idx') for evidence in evids_batch]
-                if gold_evidence_lines := entry['evidence_lines']:
-                    evidence_groups = gold_evidence_lines.split(';')
-                    pr_useable = False
-                    for group in evidence_groups:
-                        if all(line in pr_evidence_lines for line in group.split(',')):
-                            pr_useable = True
-                            break
-                    if not pr_useable:  # no gold evidence in predicted evidence
-                        print(f"Claim predictions false: {entry['claim']}")
-                        builded_ev_lines = []
-                        while len(builded_ev_lines) < 3:
-                            if len(evidence_groups) == 0:
-                                while len(builded_ev_lines) < 3:
-                                    line = pr_evidence_lines.pop()
-                                    if line not in builded_ev_lines:
-                                        builded_ev_lines.append(line)
-                            else:
-                                group = random.choice(evidence_groups)
-                                evidence_groups.remove(group)
-                                group = group.split(',')
-                                if len(builded_ev_lines) + len(group) <= 3:
-                                    builded_ev_lines.extend(group)
-                        pr_evidence_lines = builded_ev_lines
-                selected_evidence_lines.append(','.join(pr_evidence_lines))
-            except Exception as e:
-                print(f"Exception at {entry['claim']}")
-        dataset = dataset.remove_columns('selected_evidence_lines')
-        dataset = dataset.add_column('selected_evidence_lines', selected_evidence_lines)
-        dataset_dict['name'] = dataset
-
-    dataset_dict.push_to_hub('lukasellinger/fever_claim_verification_dissim-v1', private=True)
-
-main()
+dataset_dict.push_to_hub('lukasellinger/fever_claim_verification_dissim-v1', private=True)
